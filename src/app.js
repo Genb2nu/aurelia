3548d15bbd67975a0659e96d6e2af7abc1e5ed0e
Eugene Van Betonio <betonio.eugene@gmail.com>
2016-12-01 18:50:00 +0800

    Initial

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import 'bootstrap';
+
+import {inject} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+import AppRouterConfig from './router-config.js';
+
+import {FetchConfig} from 'aurelia-auth';
+@inject(Router, FetchConfig, AppRouterConfig)
+export class App {
+
+    constructor(router, fetchConfig, appRouterConfig) {
+        this.router = router;
+        this.appRouterConfig = appRouterConfig;
+        this.fetchConfig = fetchConfig;
+    }
+
+    activate() {
+        this.appRouterConfig.configure();
+        this.fetchConfig.configure();
+    }
+}
---
+++ src/auth-config.js
@@ -0,0 +1,17 @@
+var config = {
+
+  // Our Node API is being served from localhost:3001
+  baseUrl: 'http://localhost:5002',
+  // The API specifies that new users register at the POST /users enpoint.
+  signupUrl: 'users',
+  // Logins happen at the POST /sessions/create endpoint.
+  loginUrl: 'sessions/create',
+  // The API serves its tokens with a key of id_token which differs from
+  // aureliauth's standard.
+  tokenName: 'id_token',
+  // Once logged in, we want to redirect the user to the welcome view.
+  loginRedirect: '#/welcome',
+
+}
+
+export default config;
